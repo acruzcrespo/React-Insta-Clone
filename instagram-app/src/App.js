aea2bd02b9bbc54d70d18738041e8c7a2e630a15
Ash <ashley.cruz.crespo@gmail.com>
2019-05-08 11:01:41 -0400

    added the comment section to the app

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import CommentSection from './components/CommentSection/CommentSection';
+import PostContainer from './components/PostContainer/PostContainer';
 
 function App() {
   return (
     <div className="App">
       <SearchBar/>
       <CommentSection/>
-      <
+      <PostContainer/>
     </div>
   );
 }

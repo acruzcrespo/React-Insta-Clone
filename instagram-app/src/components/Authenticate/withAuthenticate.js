acce4ddbe9e1600df4aee0bc141387044e531a3d
Ash <ashley.cruz.crespo@gmail.com>
2019-05-08 15:35:18 -0400

    move to work on authenticate

---
+++ instagram-app/src/components/Authenticate/withAuthenticate.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const withAuthenticate = PostsPage => LoginPage => 
+  class extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loggedIn: false
+        }
+    }
+
+    componentDidMount() {
+        if (!localStorage.getItem('username')){
+            this.setState({ 
+                loggedIn: false 
+            });
+        } else {
+            this.setState({ 
+                loggedIn: true 
+            })
+        }
+    }

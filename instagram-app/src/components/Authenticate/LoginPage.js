dac3dddcba10aecfa081f6a75a172242ee793390
Ash <ashley.cruz.crespo@gmail.com>
2019-05-09 09:31:24 -0400

    fixing my login page

--- instagram-app/src/components/Authenticate/LoginPage.js
+++ instagram-app/src/components/Authenticate/LoginPage.js
@@ -1,4 +1,73 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const LoginPageContainer = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        width: 100vw;
+        background-image: linear-gradient(to right, #FF0099, #5c3a4f);
+`;
+
+const LoginCard = styled.form`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid silver;
+        box-shadow: 0 0 10px silver;
+        padding: 15px;
+        height: 500px;
+        width: 300px;
+        background: #fafafa;
+        opacity: 0.7;
+`;
+
+const StyledH2 = styled.h2`
+        font-family: 'billabongregular';
+        font-size: 5rem;
+        padding-bottom: 15px;
+`;
+
+const Subtitle = styled.p`
+        font-family: 'Roboto', sans-serif;
+        color: gray;
+        padding-bottom: 10px;
+        font-size: 1.1rem;
+`;
+
+const LoginInput = styled.input`
+        margin: 10px;
+        height: 35px;
+        border-top-right-radius: 8px;
+        border-top-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+        border: 1px solid silver;
+        box-shadow: 0 0 8px silver;
+        text-align: center;
+        font-size: 1rem;
+        width: 80%;
+        :focus {
+            outline: none;
+        }
+`;
+
+const LoginButton = styled.button`
+        border-radius: 8px;
+        background: rgba(95, 21, 138, 0.938);
+        border: 0px solid transparent;
+        height: 35px;
+        width: 80%;
+        margin: 20px auto;
+        color: white;
+        font-size: 1rem;
+        transition: .2s linear;
+        :hover {
+            cursor: pointer;
+        }
+`;
 
 
 
@@ -24,7 +93,6 @@ class LoginPage extends React.Component {
         localStorage.setItem('username', username);
         window.location.reload();
     }    
-
 
     render(){
     return (
